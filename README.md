75077cc5d04901a1562d0416c0fc1df4ed2a19e1
Vojtěch Týnavský <vojtech.tynavsky@seznam.cz>
2024-08-11 18:13:52 +0200

    Creating Pathfinding, Tile and Speed Contexts

--- README.md
+++
@@ -1,2 +0,0 @@
-# Find-your-path
-An app where you can get from one side to the other using the shortest path.
---
+++ pathfinding-visualizer/src/App.tsx
@@ -0,0 +1,21 @@
+import { PathfindingProvider } from "./context/PathfindingContext"
+import { SpeedProvider } from "./context/SpeedContext"
+import { TileProvider } from "./context/TileContext"
+
+function App() {
+
+
+  return (
+    <PathfindingProvider>
+      <TileProvider>
+        <SpeedProvider>
+          <h1 className="text-3xl font-bold underline h-screen w-screen bg-blue-500">
+            Hello world!
+          </h1>
+        </SpeedProvider>
+      </TileProvider>
+    </PathfindingProvider>
+  )
+}
+
+export default App
